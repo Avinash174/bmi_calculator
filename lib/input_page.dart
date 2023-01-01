308f7aa35afeccbedbdbf5352ee7f2a6042038e0
Avinash175 <avinashmagar15@gmail.com>
2023-01-01 17:27:47 +0530

    New App Created

---
+++ lib/input_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(child: Row(
+            children: [
+              Expanded(child: new Reusable(color: Color(0xFF1D1E33),),),
+              Expanded(child:Container(
+
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0),
+                  )
+              ),),
+            ],
+          ),),
+          Expanded(child: new Reusable(color: Color(0xFF1D1E33),)),
+          Expanded(child: Row(
+            children: [
+              Expanded(child: new Reusable(color: Color(0xFF1D1E33),)),
+              Expanded(child: new Reusable(color: Color(0xFF1D1E33)),
+              )],
+          )),
+        ],
+      ),
+     
+    );
+  }
+}
+class Reusable extends StatelessWidget {
+   Reusable({required this.color});
+
+  Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
