e79ac4580512e607127c6108af99c81cda38ba10
Avinash175 <avinashmagar15@gmail.com>
2023-01-04 12:48:17 +0530

    New Changes Added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,40 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable.dart';
 import 'icon_content.dart';
+import 'constant.dart';
 
 
-const bottomContainerHeight=80.0;
-const activeCardColor=Color(0xFF1D1E33);
-const inactiveColor=Color(0xFF111328);
-const bottomContainerColor=Color(0xFFEB1555);
+
+enum Gender {
+  male,
+  female,
+}
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
-  Color malecardColor=inactiveColor;
-  Color femaleCardColor=inactiveColor;
+  int height=100;
+  Color malecardColor = inactiveColor;
+  Color femaleCardColor = inactiveColor;
 
-  void updateColor(int gender){
-    if(gender==1){
-      if(malecardColor==inactiveColor) {
+  void updateColor(Gender selectGender) {
+    if (selectGender == Gender.male) {
+      if (malecardColor == inactiveColor) {
         malecardColor = activeCardColor;
-        femaleCardColor=inactiveColor;
-      }else{
-        malecardColor=inactiveColor;
-
+        femaleCardColor = inactiveColor;
+      } else {
+        malecardColor = inactiveColor;
       }
     }
-    if(gender==2){
-      if(femaleCardColor==inactiveColor){
-        femaleCardColor=activeCardColor;
-        malecardColor=inactiveColor;
-      }else{
-        femaleCardColor=inactiveColor;
+    if (selectGender == Gender.female) {
+      if (femaleCardColor == inactiveColor) {
+        femaleCardColor = activeCardColor;
+        malecardColor = inactiveColor;
+      } else {
+        femaleCardColor = inactiveColor;
       }
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,19 +48,20 @@ class _InputPageState extends State<InputPage> {
         centerTitle: true,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Row(
               children: [
                 Expanded(
                   child: GestureDetector(
-                    onTap: (){
+                    onTap: () {
                       setState(() {
-                        updateColor(1);
+                        updateColor(Gender.male);
                       });
                     },
                     child: Reusable(
-                      color:malecardColor,
+                      color: malecardColor,
                       cardChild: IconContent(
                         icon: FontAwesomeIcons.mars,
                         label: 'MALE',
@@ -66,9 +71,9 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: GestureDetector(
-                    onTap: (){
+                    onTap: () {
                       setState(() {
-                        updateColor(2);
+                        updateColor(Gender.female);
                       });
                     },
                     child: Reusable(
@@ -86,9 +91,37 @@ class _InputPageState extends State<InputPage> {
           Expanded(
               child: Reusable(
             color: activeCardColor,
-            cardChild: Row(
+            cardChild: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Text(
+                  'HEIGHT',
+                  style: labelStyle,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Text(height.toString(), style: numberStyle),
+                    Text(
+                      'cm',
+                      style: labelStyle,
+                    )
+                  ],
+                ) ,
+                Slider(value: height.toDouble(),
+                    min: 120.0,
+                    max: 200.0,
+                    activeColor: Color(0xFFEB1555),
+                    inactiveColor: Color(0xFF8D8E98),
+                    
+                    onChanged: (double newValue){
 
+                      setState(() {
+                       height=newValue.round();
+                      });
+                    }),
               ],
             ),
           )),
@@ -116,9 +149,6 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
-    );
+      );
   }
 }
-
-
-
